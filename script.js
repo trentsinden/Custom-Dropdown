aa023ca6e6417518b3869edeb3bf235342ea4a9e
Trent Sinden <trentsinden@gmail.com>
2021-01-29 14:40:13 -0500

    finished

---
+++ script.js
@@ -0,0 +1,8 @@
+import Select from './select.js'
+
+const selectElements = document.querySelectorAll("[data-custom]")
+
+selectElements.forEach(selectElement => {
+    new Select(selectElement)
+})
+
